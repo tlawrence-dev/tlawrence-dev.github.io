60756dd76b6abac0c9270924d4a8c2f383ebf978
tlawrence-dev <64038265+tlawrence-dev@users.noreply.github.com>
2020-04-20 22:27:59 +0100

    Fixed background sizing

--- portal/js/main.js
+++ portal/js/main.js
@@ -2,5 +2,8 @@ $( document ).ready(function() {
     
     var bgImgArray = ['img/bg_001.jpg','img/bg_002.jpg','img/bg_003.jpg','img/bg_004.jpg'],
     selectBG = bgImgArray[Math.floor(Math.random() * bgImgArray.length)];
-    $('body').css('background', 'url(' + selectBG + ')')
+    $('body').css('background', 'url(' + selectBG + ')');
+    $('body').css('background-position', 'center');
+    $('body').css('background-repeat', 'no-repeat');
+    $('body').css('background-size', 'cover');
 });
