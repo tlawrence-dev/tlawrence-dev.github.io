7351a43dd11d900e6681027f6ad94a8b5122322a
tlawrence-dev <64038265+tlawrence-dev@users.noreply.github.com>
2020-04-20 21:14:40 +0100

    Update main.js

--- portal/js/main.js
+++ portal/js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     
-    var bgImgArray = ['/img/bg_001.jpg','/img/bg_002.jpg','/img/bg_003.jpg','/img/bg_004.jpg'],
+    var bgImgArray = ['img/bg_001.jpg','img/bg_002.jpg','img/bg_003.jpg','img/bg_004.jpg'],
     selectBG = bgImgArray[Math.floor(Math.random() * bgImgArray.length)];
 
     $('body').css('background', 'url(' + selectBG + ')')
